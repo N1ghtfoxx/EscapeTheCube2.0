994b4aaf7042adc45f0e5ae1a563577136525923
BTLV444\Osman <2024.gme.osmansen@gmail.com>
2026-02-18 13:28:29 +0100

    ADD HomeScreen that one gets to when starting the game

---
+++ Assets/Scripts/HomeScreen/HomeScreen.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomeScreen : MonoBehaviour
+{
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene("StartScreen");
+    }
+    
+}
