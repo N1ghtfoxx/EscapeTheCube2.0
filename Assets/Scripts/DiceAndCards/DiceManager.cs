d3406f90b2cae10e8cc533ae3bd62552885e7750
BTLV444\Osman <2024.gme.osmansen@gmail.com>
2026-02-23 15:28:53 +0100

    EDT Dicing Area to it is now in the playfield ADD BU-StartScreen 1 scene as backup because I made edits in the StartScreenScene ADD Fixes Scene

--- Assets/Scripts/DiceAndCards/DiceManager.cs
+++ Assets/Scripts/DiceAndCards/DiceManager.cs
@@ -60,9 +60,9 @@ public class DiceManager : MonoBehaviour
             diceRb.isKinematic = false;
             diceRb.linearVelocity = Vector3.zero;
             diceRb.angularVelocity = Vector3.zero;
-            diceRb.transform.position = new Vector3(throwPosition.position.x, throwPosition.position.y, throwPosition.position.z + Random.Range(-3.5f, 3.5f));
+            diceRb.transform.position = new Vector3(throwPosition.position.x + Random.Range(-3.5f, 3.5f), throwPosition.position.y, throwPosition.position.z);
             diceRb.transform.rotation = Random.rotation;
-            Vector3 forceDirection = new Vector3(Random.Range(-3f, -1f), verticality, Random.Range(-1f, 1f)).normalized;
+            Vector3 forceDirection = new Vector3(Random.Range(-2f, 2f), verticality, Random.Range(3f, 1f)).normalized;
             diceRb.AddForce(forceDirection * diceForce, ForceMode.Impulse);
             diceRb.AddTorque(Random.insideUnitSphere * diceTorque, ForceMode.Impulse);
         }
