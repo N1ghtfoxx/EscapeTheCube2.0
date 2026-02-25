a8364055deae411977ecbd4d5f61d696a60ee444
BTLV444\Osman <2024.gme.osmansen@gmail.com>
2026-02-25 15:40:31 +0100

    FIX scene Name to change to change to in code

--- Assets/Scripts/HomeScreen/HomeScreen.cs
+++ Assets/Scripts/HomeScreen/HomeScreen.cs
@@ -6,7 +6,7 @@ public class HomeScreen : MonoBehaviour
     
     public void StartGame()
     {
-        SceneManager.LoadScene("StartScreen");
+        SceneManager.LoadScene("CharacterSelection");
     }
     
 }
